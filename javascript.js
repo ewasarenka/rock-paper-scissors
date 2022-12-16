85ab2b5ee080ab52dbc36e79dee24baadcee2a41
ewa <kiwinaolx@gmail.com>
2022-12-16 04:06:46 +0100

    Changed playRound() to searchString method

--- javascript.js
+++ javascript.js
@@ -1,39 +1,54 @@
 const basicValues = ['Rock','Paper','Scissors'];
+let searchString = ['Rock','Paper','Scissors','Rock'];
+searchString = searchString.join('');
 
-//begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
     //losowanie
-    let randomNumber = Math.floor(Math.random()*basicValues.length);
+    let randomNumber = Math.floor(Math.random()*3);
     return basicValues[randomNumber];
 };
 
-//Write a function that plays a single round of Rock Paper Scissors. 
-//The function should take two parameters - the playerSelection and computerSelection
-//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-function playRound(playerSelection, computerSelection) {
-    let result;
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
 
-    if ((playerSelection == basicValues[0] && computerSelection == basicValues[2]) || (playerSelection == basicValues[1] && computerSelection == basicValues[0]) || (playerSelection == basicValues[2] && computerSelection == basicValues[1])) {
-        return `You Won! ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection == computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    let combinedValues = computerSelection + playerSelection;
+    let result;
+    if (playerSelection == computerSelection) { 
         result = 'It\'s a tie!';
+    } else if (searchString.search(combinedValues) > -1 ) {
+        return `You Won! ${playerSelection} beats ${computerSelection}`;
     } else {
         result = `You Lose! ${computerSelection} beats ${playerSelection}`;
     };
-    //return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
     return result;
     }
 
+    const Div = document.querySelector('.results');
+    const Score = document.querySelector('.score');
+    const Buttons = document.querySelectorAll('button');
+    let computerScore = 0;
+    let playerScore = 0;
+
+    Buttons.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            let roundResult = playRound(btn.innerHTML,getComputerChoice());
+            Div.textContent = roundResult;
+            if(roundResult.search('Won') > -1) {
+                playerScore++ ;
+            } else if (roundResult.search('Lose') > -1) {
+                computerScore++ ;
+            }
+            Score.textContent = `You ${playerScore} : ${computerScore} Computer`
+        }); 
+    });
+    
+   
+
 // function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-   function game() {
+/*    function game() {
     for (let i = 0; i < 5; i++) {
    const playerSelection = prompt('Rock, paper or scissors?','rock');
    const computerSelection = getComputerChoice();
    console.log(playRound(playerSelection, computerSelection));
-    }}
-
-    game();
-
-
+    }} */
+/* 
+    game(); */
